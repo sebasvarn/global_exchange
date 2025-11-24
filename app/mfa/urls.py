7ee7bb86147d5b58c86b1e6ee1f54384f840e506
ilsongon <ilsongonzalez015@gmail.com>
2025-11-24 08:35:18 -0300

    feature: IS2-78: Implementación MFA para el tramite de transacciones

--- app/mfa/urls.py
+++ app/mfa/urls.py
@@ -5,5 +5,7 @@ app_name = 'mfa'
 
 urlpatterns = [
     path('generate/', views.generate_otp_view, name='generate_otp'),
-    path('verify/', views.verify_otp_view, name='verify'),
+    path('verify/', views.verify_otp, name='verify_otp'),
+    path('api/generate/', views.generate_otp_view, name='generate_otp_api'),
+    path('api/verify-tauser-transaction/', views.verify_tauser_transaction_otp, name='api_verify_tauser_otp'),
 ]
